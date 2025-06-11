b1c2f42a225944010c3e7582f76216e2a3736b16
TanmaySamdani19 <tanmaysamdani12345@gmail.com>
2025-06-11 19:54:36 +0100

    integrating with llm

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,32 +5,31 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_openai import ChatOpenAI
 from langchain_ollama import ChatOllama
 from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agents import lookup as linkedin_lookup_agent
 
 # Load environment variables from .env file
 load_dotenv()
 
-information = """
-"""
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name = name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url = linkedin_username)
 
-if __name__ == "__main__":
-    print("Hello LangChain!")
-    print(f"API Key: {os.environ.get('OPENAI_API_KEY', 'API key not found')}")
     summary_template = """
-    Given the Linkedin information {information} about a person, I want you to create:
-    1. A short summary of the person
-    2. Two interesting facts about the person
+    given the Linkedin Information {information} about a person I want you to create:
+    1. A short summary
+    2. two interesting facts about them
     """
 
     summary_prompt_template = PromptTemplate(
-        input_variables=["information"], template=summary_template
+        input_variables= ["information"], template= summary_template
     )
 
-
-    # llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
-    llm = ChatOllama(model="llama3")
-
-    chain = summary_prompt_template | llm | StrOutputParser()
-    linkedin_data = scrape_linkedin_profile(linkedin_profile_url = "https://www.linkedin.com/in/tanmay-samdani-96606b205/",mock=True)
-    res = chain.invoke(input={"information": linkedin_data})
-
+    llm = ChatOpenAI(temperature=0, model_name = "gpt-4o-mini")
+    chain = summary_prompt_template | llm
+    res = chain.invoke(input = {"information": linkedin_data})
     print(res)
+
+if __name__ == "__main__":
+    load_dotenv()
+    print("Ice breaker Enter")
+    ice_break_with(name="Tanmay Samdani")
